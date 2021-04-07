a68b53dccf3efb847696249f50597c9a4ae801cc
Yukai Huang <yukaihuangtw@gmail.com>
2021-04-07 21:33:26 +0800

    Initial commit

---
+++ tokens.js
@@ -0,0 +1,67 @@
+/**
+ * Extracted from https://highlightjs.readthedocs.io/en/latest/css-classes-reference.html
+ */
+
+  export const GENERAL_TOKENS = { 
+    KEYWORD: 'keyword',
+    BUILT_IN: 'built_in',
+    TYPE: 'type',
+    LITERAL: 'literal',
+    NUMBER: 'number',
+    OPERATOR: 'operator',
+    PUNCTUATION: 'punctuation',
+    REGEXP: 'regexp',
+    STRING: 'string',
+    SUBST: 'subst',
+    SYMBOL: 'symbol',
+    CLASS: 'class',
+    FUNCTION: 'function',
+    VARIABLE: 'variable',
+    TITLE: 'title',
+    PARAMS: 'params',
+    COMMENT: 'comment',
+    DOCTAG: 'doctag',
+  }
+
+export const META_TOKENS = {
+  META: 'meta',
+  META_KEYWORD: 'meta-keyword',
+  META_STRING: 'meta-string',
+}
+
+export const TAG_ATTR_CONF_TOKENS = {
+  SECTION: 'section',
+  TAG: 'tag',
+  NAME: 'name',
+  BUILTIN_NAME: 'builtin-name',
+  ATTR: 'attr',
+  ATTRIBUTE: 'attribute',
+}
+
+export const TEXT_MARKUP_TOKENS = {
+  BULLET: 'bullet',
+  CODE: 'code',
+  EMPHASIS: 'emphasis',
+  STRONG: 'strong',
+  FORMULA: 'formula',
+  LINK: 'link',
+  QUOTE: 'quote',
+}
+
+export const CSS_TOKENS = {
+  SELECTOR_TAG: 'selector-tag',
+  SELECTOR_ID: 'selector-id',
+  SELECTOR_CLASS: 'selector-class',
+  SELECTOR_ATTR: 'selector-attr',
+  SELECTOR_PSEUDO: 'selector-pseudo',
+}
+
+export const TEMPLATE_TOKENS = {
+  TEMPLATE_TAG: 'template-tag',
+  TEMPLATE_VARIABLE: 'template-variable',
+}
+
+export const DIFF_TOKENS = { 
+  TEMPLATE_TAG: 'template-tag',
+  TEMPLATE_VARIABLE: 'template-variable',
+}
